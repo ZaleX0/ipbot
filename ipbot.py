2ad30108f8abc5d9044aa8fc4a00ed23dc30e47e
Szymon Głowacki <pgszymon@gmail.com>
2024-06-03 16:34:58 +0200

    Add mc version to discord message

--- ipbot.py
+++ ipbot.py
@@ -25,6 +25,7 @@ file.close()
 cfg = json.load(open('ipbot_cfg.json'))
 TOKEN = cfg['TOKEN']
 CHANNEL_ID = cfg['CHANNEL_ID']
+MC_VERSION = cfg['MC_VERSION']
 
 intents = discord.Intents.default()
 intents.message_content = False
@@ -34,7 +35,7 @@ client = commands.Bot(intents = intents, command_prefix = '!')
 async def on_ready():
     channel = client.get_channel(CHANNEL_ID)
     await channel.purge(limit=3)
-    await channel.send(IP)
+    await channel.send(f'Wersja Minecraft: `{MC_VERSION}`\nAdres: `{IP}`')
     await client.close()
 
 client.run(TOKEN)
